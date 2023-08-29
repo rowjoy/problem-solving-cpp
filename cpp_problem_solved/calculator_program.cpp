af5197b21fe060a76100afc02abf14dd4f95c00d
NecJoy <joy@primotechltd.com>
2023-08-29 13:35:35 +0600

    Calculator program

---
+++ cpp_problem_solved/calculator_program.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    // terminal input cin >>a>>b
+    // show output (cout)
+
+    int a = 10, b = 12, c, d, f;
+    c = a * b;
+    d = b - a;
+    float e = (float) b / f;
+    cout << "Multipla value : "  << d << "\n";
+    cout << "Sub value : " << c << "\n";
+    cout << "___" << e << "\n";
+    
+
+    // value swaip in a & b 
+    cout << "a value is " << a << "\n";
+    cout << "b value is " << b << "\n";
+    a = a + b ;
+    b = a - b;
+    a = a - b;
+    cout << "a value is " << a << "\n";
+    cout << "b value is " << b << "\n";
+
+
+
+}
