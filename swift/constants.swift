5385d22e3486acb0d895c111de5718d51b3105cf
rowjoy <rowjoy202@gmail.com>
2024-09-29 19:29:58 +0600

    Swift - 1

---
+++ swift/constants.swift
@@ -0,0 +1,19 @@
+// Constants refer to fixed values 
+
+let number = 10;
+
+// note: change 'let' to 'var' to make it mutable
+print("Number : ", number);
+
+//number = 23; // Show error  [change 'let' to 'var' to make it mutable]
+
+print("Number 2 :", number);
+
+
+// Const varibale use multiple varibale 
+
+let userName = "Jamirul islam" , userAge = 32 , userIncome = 40.00, userAddress = "Dhaka Bangladesh";
+
+print("User Name :" , userName);
+
+
