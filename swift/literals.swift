4dcbb7403dc511a39f26d23c0ac36e7117b8a7de
rowjoy <rowjoy202@gmail.com>
2024-09-30 17:41:34 +0600

    swift -2

---
+++ swift/literals.swift
@@ -0,0 +1,28 @@
+
+// Swift integer literals 
+/**
+Decimal to  integer == Positive number 
+Binary to intger  = Ob
+Octal to intger = 0o
+Hexadecimal to intger = 0x
+
+
+*/
+
+// Code Example 
+
+let number = 32;
+
+let binaryNumber = 0b101010100001000;
+let otcalNumber = 0o532;
+let hexadecimalNumber = 0x5C;
+
+
+print("number : \(number)");
+print("binaryNumber : \(binaryNumber)");
+print("otcalNumber : \(otcalNumber)");
+print("Hexadecimal : \(hexadecimalNumber)");
+
+//https://www.tutorialspoint.com/swift/swift_literals.htm
+
+
