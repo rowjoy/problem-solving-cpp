de9c3b1ad6609455daa2e73b272a5a912061bc37
rowjoy <rowjoy202@gmail.com>
2025-02-05 19:47:58 +0600

    Count the divisors of a number

---
+++ dart/Count the divisors of a number.dart
@@ -0,0 +1,21 @@
+void main (){
+   divisors(500000);
+}
+
+
+int divisors(n) {
+  // your code here
+  if( n == null || n == 0)return 0;
+  int result = 0;
+  int i = 1;
+  while( i <=  n){
+   if (n % i == 0){
+      result ++;
+   }
+   i++;
+  }
+  
+
+  print("result ${result}");
+  return result;
+}
